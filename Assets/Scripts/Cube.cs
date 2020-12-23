b177cb04797bc9ede936a79273b2a73b72b4f5f0
Ufuk Güney <38943892+ufukkguney@users.noreply.github.com>
2020-12-24 00:23:17 +0300

    Grid Data
    Game manager grid amount değiştirerek gridler test edilebilir

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject xObject;
+    private GameManager gameManager;
+    private GameObject grids;
+
+
+    List<GameObject> xObjects;
+
+
+    void Start()
+    {
+        xObject = transform.Find("XObject").gameObject;
+        gameManager = FindObjectOfType<GameManager>();
+        grids = gameManager.transform.Find("Grids").gameObject;
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (!xObject.activeInHierarchy)
+        {
+            xObjects = new List<GameObject>();
+            int counter = 0;
+            string[] minmax = transform.gameObject.name.Split(',');
+            int width = int.Parse(minmax[0]);
+            int height = int.Parse(minmax[1]);
+
+            int tempIncWidth = width + 1;
+            int tempDecWidth = width - 1;
+
+            int tempIncHeight = height + 1;
+            int tempDecHeight = height - 1;
+
+            AddToList(tempDecWidth + "," + tempDecHeight);
+            AddToList(tempDecWidth + "," + height);
+            AddToList(tempDecWidth + "," + tempIncHeight);
+            AddToList(width + "," + tempDecHeight);
+            AddToList(width + "," + height);
+            AddToList(width + "," + tempIncHeight);
+            AddToList(tempIncWidth + "," + tempDecHeight);
+            AddToList(tempIncWidth + "," + height);
+            AddToList(tempIncWidth + "," + tempIncHeight);
+
+
+            for (int i = 0; i < xObjects.Count; i++) // just for count
+            {
+                if (xObjects[i].transform.Find("XObject").gameObject.activeInHierarchy)
+                {
+                    counter++;
+                }
+            }
+
+            if (counter >= 3)
+            {
+                for (int i = 0; i < xObjects.Count; i++)
+                {
+                    if (xObjects[i].transform.Find("XObject").gameObject.activeInHierarchy)
+                    {
+                        xObjects[i].transform.Find("XObject").gameObject.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                xObject.SetActive(true);
+            }
+        }
+        
+    }
+    private void AddToList(string ObjectName)
+    {
+        for (int i = 0; i < grids.transform.childCount; i++)
+        {
+            if (grids.transform.GetChild(i).gameObject.name.Contains(ObjectName))
+            {
+                GameObject neighbor = grids.transform.Find(ObjectName).gameObject;
+                xObjects.Add(neighbor);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int gridAmount;
+
+    private int gridTotalSize = 10;
+
+    private float cubeScale;
+
+    public GameObject gridCube;
+
+
+
+    void Start()
+    {
+        GridInstatiate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GridInstatiate()
+    {
+        cubeScale = (float)gridTotalSize / gridAmount;
+
+        for (float i = 0; i < gridAmount; i++)
+        {
+            for (float j = 0; j < gridAmount; j++)
+            {
+                GameObject cube =  Instantiate(gridCube, new Vector3(j * cubeScale, i * cubeScale, 0), Quaternion.identity, transform.Find("Grids"));
+                cube.transform.localScale = new Vector3(cubeScale, cubeScale, cubeScale);
+                cube.transform.gameObject.name = "" + j + "," + i + "";
+            }
+        }
+    }
+}
